60d981e275992d38d62da9e9805e50ede58d3f84
Francis Jairo Matías Rosario <jairo_1829@hotmail.com>
2020-05-01 19:33:02 -0400

    Calculadora de prestamos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ResultadosComponent } from './componentes/resultados/resultados.component';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , component: CalculadoraComponent },
+{path: 'resultados', component: ResultadosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
+import { ResultadosComponent } from './componentes/resultados/resultados.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    ResultadosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
